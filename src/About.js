dfccd7fb5c11986582ae4c10296f7f57c160ae6e
y <yasir@yasirs-MacBook-Pro.local>
2021-09-12 02:52:45 +0530

    changes

--- src/About.js
+++ src/About.js
@@ -36,14 +36,14 @@ export const About = () => {
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="3000">
-      <img src={photo4} class="d-block w-100" alt="..."/>
+      <img src={photo5} class="d-block w-100" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
         
         
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="3000">
-      <img src={photo5} class="d-block w-100" alt="..."/>
+      <img src={photo4} class="d-block w-100" alt="..."/>
       <div class="carousel-caption d-none d-md-block">
         <h1></h1>
         <p></p>
